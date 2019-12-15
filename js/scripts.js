a7dace9f93a5a5fa6584957173b16fd9a9e806d1
mdrokun <rukonuddin623@gmail.com>
2019-12-15 23:46:34 +0600

    Completed 2nd revision.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+$(window).on('load', function () {
+    $('.loader__img').fadeOut(500, function () {
+        $('.loader').fadeOut(750);
+    });
+});
+
 $(document).ready(function () {
     $('.nav-button').click(function () {
         $('.nav-button').toggleClass('transform-x')
@@ -9,13 +15,27 @@ $(document).ready(function () {
         play: 4000
     });
 
+    // Typed effect
+    var typedText = $('.typed').attr('data-text');
+    var typedSeparated = typedText.split(',');
     var typed = new Typed('.typed', {
-        strings: ['Frontend Developer', 'WordPress Developer', 'UI/UX Expert'],
+        strings: typedSeparated,
         typeSpeed: 70,
         loop: true,
         startDelay: 1000,
         showCursor: false
     });
+
+
+    $('.navbar-brand').click(smoothBodyScroll);
+    $('.navbar-nav li a').click(smoothBodyScroll);
+
+    function smoothBodyScroll(e) {
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: $(this.hash).offset().top - 60
+        }, 'slow');
+    }
 
     $(window).scroll(function () {
         var position = $(this).scrollTop();
@@ -24,6 +44,16 @@ $(document).ready(function () {
         } else {
             $('.nav-menu').removeClass('custom-nav')
         }
+
+        $('.navbar-nav li a').each(function () {
+
+            var sectionOffset = $(this.hash).offset().top - 110;
+
+            if (sectionOffset <= position) {
+                $(this).parent().siblings().children().removeClass('nav-active');
+                $(this).addClass('nav-active');
+            }
+        })
     });
 })
 
@@ -98,8 +128,90 @@ testimonialOwl.owlCarousel();
 // Go to the next item
 $('.testimonials-section__owl-next').click(function () {
     testimonialOwl.trigger('next.owl.carousel');
-})
+});
 // Go to the previous item
 $('.testimonials-section__owl-prev').click(function () {
     testimonialOwl.trigger('prev.owl.carousel', [300]);
-})
+});
+
+
+/* =====================================
+    Animations
+======================================== */
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var bodyOffset = $(this).scrollTop();
+        console.log(bodyOffset)
+
+        // var aboutTitleOffset = $('#about').offset().top - 500;
+
+        // About section
+        if (bodyOffset >= 150) {
+            $('#about .section-title').addClass('animate-heading');
+        } else {
+            $('#about .section-title').removeClass('animate-heading');
+        }
+
+        if (bodyOffset >= 135) {
+            $('.about__detail').addClass('move-from-right');
+            $('.about__img').addClass('move-from-left img-border-anim');
+        } else {
+            $('.about__detail').removeClass('move-from-right');
+            $('.about__img').removeClass('move-from-left img-border-anim');
+        }
+
+        // Skills section
+        if (bodyOffset >= 930) {
+            $('#skills .section-title').addClass('animate-heading');
+        } else {
+            $('#skills .section-title').removeClass('animate-heading');
+        }
+
+        // Works section
+        if (bodyOffset >= 1550) {
+            $('#works .section-title').addClass('animate-heading');
+        } else {
+            $('#works .section-title').removeClass('animate-heading');
+        }
+
+        // Price section
+        if (bodyOffset >= 2680) {
+            $('#pricing .section-title').addClass('animate-heading');
+            $('.card-1').addClass('move-from-left');
+            $('.card-2').addClass('move-from-bottom');
+            $('.card-3').addClass('move-from-right');
+        } else {
+            $('#pricing .section-title').removeClass('animate-heading');
+            $('.card-1').removeClass('move-from-left');
+            $('.card-2').removeClass('move-from-bottom');
+            $('.card-3').removeClass('move-from-right');
+        }
+
+        // Testimonials section
+        if (bodyOffset >= 3565) {
+            $('#testimonials .section-title').addClass('animate-heading');
+        } else {
+            $('#testimonials .section-title').removeClass('animate-heading');
+        }
+
+        // Contact section
+        if (bodyOffset >= 4220) {
+            $('#contact .section-title').addClass('animate-heading');
+            $('#contact-card-1').addClass('move-from-left');
+            $('#contact-card-2').addClass('scale-down');
+            $('#contact-card-3').addClass('move-from-right');
+            $('#contact-form').addClass('move-from-bottom');
+        } else {
+            $('#contact .section-title').removeClass('animate-heading');
+            $('#contact-card-1').removeClass('move-from-left');
+            $('#contact-card-2').removeClass('scale-down');
+            $('#contact-card-3').removeClass('move-from-right');
+            $('#contact-form').removeClass('move-from-bottom');
+        }
+
+    });
+
+});
+/* =====================================
+    End of Animations
+======================================== */
